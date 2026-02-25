ace73d5bfb11dad011d0535e93ce53e15d173f73
GaetanP <gaetan.puiseux@epfedu.fr>
2026-02-25 15:50:59 +0100

    ajout Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Home } from './home/home';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar], // 👈 AJOUTE Home ICI
+  imports: [RouterOutlet, Navbar, Footer], // 👈 AJOUTE Home ICI
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
