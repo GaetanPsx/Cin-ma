aef858fb49818d3f81753bcf1d28776ee27344a0
GaetanP <gaetan.puiseux@epfedu.fr>
2026-03-03 16:31:55 +0100

    filtre

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
-
 import { MoviesApi } from '../services/movies-api';
 import { MovieCard } from './movie-card/movie-card';
-
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
+type SortMode = 'rating-desc' | 'rating-asc';
 
 @Component({
   selector: 'app-home',
@@ -18,25 +17,33 @@ import { map } from 'rxjs/operators';
 export class Home {
   private readonly moviesApi = inject(MoviesApi);
 
-  // Liste complète
   movies$ = this.moviesApi.getMovies();
 
-  // Recherche (source)
   private readonly search$ = new BehaviorSubject<string>('');
+  private readonly sort$ = new BehaviorSubject<SortMode>('rating-desc');
+  onSort(value: string) {
+  this.sort$.next(value as SortMode);
+  }
+  filteredMovies$ = combineLatest([this.movies$, this.search$, this.sort$]).pipe(
+  map(([movies, search, sort]) => {
 
-  // Liste filtrée (ce que tu affiches)
-  filteredMovies$ = combineLatest([this.movies$, this.search$]).pipe(
-    map(([movies, search]) => {
-      const q = search.trim().toLowerCase();
-      if (!q) return movies;
+    const q = search.trim().toLowerCase();
 
-      return movies.filter((m) => {
-        const title = (m.title ?? '').toLowerCase();
-        const director = (m.director ?? '').toLowerCase();
-        return title.includes(q) || director.includes(q);
-      });
+    let result = !q ? movies : movies.filter(m => {
+      const title = (m.title ?? '').toLowerCase();
+      const director = (m.director ?? '').toLowerCase();
+      return title.includes(q) || director.includes(q);
     })
-  );
+
+    result = [...result].sort((a, b) => {
+      const ra = a.rate ?? 0;
+      const rb = b.rate ?? 0;
+      return sort === 'rating-asc' ? ra - rb : rb - ra;
+    });
+
+    return result;
+  })
+);
 
   onSearch(value: string) {
     this.search$.next(value);
